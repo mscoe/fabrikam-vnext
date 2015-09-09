384a8436571c0a5c24400de6032c888961874e01
Karthik H Raju <karthik.h.raju@gmail.com>
2015-09-09 12:59:40 +0530

    Added few lines for repository

---
+++ src/Fabrikam.Command.Repository/DapperHelpers/GetSqlQuery.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Fabrikam.Command.Repository.DapperHelpers
+{
+    public class GetSqlQuery
+    {
+
+        public static string Insert(object parameters, string tableName)
+        {
+            var colums = new List<string>();
+            foreach (var colum in parameters.GetType().GetProperties())
+            {
+                var add = true;
+
+                var keys = GetPrimaryKey(parameters);
+
+                if (keys.Contains(colum.Name))
+                {
+                    add = false;
+                }
+                foreach (var attr in colum.CustomAttributes)
+                {
+                    if (attr.AttributeType == typeof(IgnoredAttribute))
+                    {
+                        add = false;
+                    }
+                }
+                if (add) colums.Add(colum.Name);
+            }
+
+
+
+            var sql = "INSERT INTO [" + tableName + "] ({0}) VALUES(@{1});SELECT CAST(SCOPE_IDENTITY() AS INT);";
+            var frmsql = String.Format(sql, String.Join(",", colums), String.Join(",@", colums));
+            return frmsql;
+        }
+
+        public static string Update(object parameters, string tableName)
+        {
+            var colums = new List<string>();
+            var keys = GetPrimaryKey(parameters);
+            foreach (var colum in parameters.GetType().GetProperties())
+            {
+                var add = true;
+
+                if (keys.Contains(colum.Name))
+                {
+                    add = false;
+                }
+                foreach (var attr in colum.CustomAttributes)
+                {
+                    if (attr.AttributeType == typeof(IgnoredAttribute))
+                    {
+                        add = false;
+                    }
+                }
+                if (add) colums.Add(colum.Name);
+            }
+            var sb = new StringBuilder();
+            sb.Append("UPDATE [" + tableName + "]  SET ");
+            for (var i = 0; i < colums.Count(); i++)
+            {
+                sb.AppendFormat("[{0}]=@{1}", colums[i], colums[i]);
+                //while not last element
+                if (i < colums.Count() - 1)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            sb.Append(" WHERE [" + keys.First() + "]= @" + keys.First());
+            if (keys.Length > 1)
+            {
+                sb.Append(" AND " + string.Join(" AND ", keys.Skip(1).Select(x => string.Format("{0}=@{1}", x, x).ToArray())));
+            }
+
+            sb.Append("SELECT * FROM [" + tableName + "]");
+            sb.Append(" WHERE [" + keys.First() + "]= @" + keys.First());
+            if (keys.Length > 1)
+            {
+                sb.Append(" AND " + string.Join(" AND ", keys.Skip(1).Select(x => string.Format("{0}=@{1}", x, x).ToArray())));
+            }
+
+            return sb.ToString();
+        }
+
+        public static string Select(string tableName)
+        {
+            var sql = "SELECT * FROM [" + tableName + "]";
+            return sql;
+        }
+
+        public static string DeleteById(string tableName)
+        {
+            var sql = "DELETE FROM [" + tableName + "] WHERE " + tableName + "Id=@Id";
+            return sql;
+        }
+
+        public static string Find(object parameters, string tableName)
+        {
+
+            throw new Exception();
+        }
+
+
+        public static string[] GetPrimaryKey(object parameter)
+        {
+            Type type = parameter.GetType();
+            var attribs = (PrimaryKeyAttribute[])type.GetCustomAttributes(typeof(PrimaryKeyAttribute), false);
+            var retVal = attribs.Select(x => x.ColumnName);
+            return retVal.ToArray();
+        }
+
+    }
+}
---
+++ src/Fabrikam.Command.Repository/GenericRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fabrikam.Command.Repository.Interfaces;
+
+namespace Fabrikam.Command.Repository
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class GenericRepository<T> : DbContext, IGenericRepository<T>
+    {
+        private readonly string _tableName;
+        private readonly IDbConnection dbConnection;
+        private object _parameters;
+
+        protected GenericRepository(string connectionName, string tableName) : base(connectionName)
+        {
+            base.ConnectionName = connectionName;
+            _tableName = tableName;
+            dbConnection = DbConnectionAsync;
+        }
+
+        internal dynamic Mapping(T obj)
+        {
+            return obj;
+        }
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            var frmsql = GetSqlQuery.Select(_tableName);
+            var list = await dbConnection.QueryAsync<T>(frmsql, transaction: DbTransaction);
+            return list;
+
+        }
+
+        public virtual async Task<T> UpdateAsync(T obj)
+        {
+            _parameters = (object)Mapping(obj);
+            var frmsql = GetSqlQuery.Update(_parameters, _tableName);
+            var result = await dbConnection.QueryAsync<T>(frmsql, _parameters, transaction: DbTransaction);
+            return result.First();
+        }
+
+        public virtual async Task<bool> DeleteByIdAsync(int id)
+        {
+            var frmsql = GetSqlQuery.DeleteById(_tableName);
+            var list = await dbConnection.QueryAsync<T>(frmsql, new { Id = id }, transaction: DbTransaction);
+            return Equals(list.FirstOrDefault(), 0);
+        }
+
+        public virtual async Task<int> AddAsync(T obj)
+        {
+            _parameters = (object)Mapping(obj);
+            var frmsql = GetSqlQuery.Insert(_parameters, _tableName);
+            var result = await dbConnection.QueryAsync<int>(frmsql, _parameters, transaction: DbTransaction);
+            return result.FirstOrDefault();
+        }
+
+        public Task<T> FindByIdAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<T> FindAsync(T obj)
+        {
+            _parameters = (object)Mapping(obj);
+            var frmsql = GetSqlQuery.Find(_parameters, _tableName);
+            var result = await dbConnection.QueryAsync<T>(frmsql, _parameters, transaction: DbTransaction);
+            return result.FirstOrDefault();
+        }
+
+
+
+    }
+}
