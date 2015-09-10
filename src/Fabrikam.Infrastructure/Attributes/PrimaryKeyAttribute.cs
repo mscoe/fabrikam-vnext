15e4b5785c15485d67d7fae0d7849c0905ac6008
Karthik H Raju <karthik.h.raju@gmail.com>
2015-09-10 15:39:23 +0530

    Adding missed files

---
+++ src/Fabrikam.Infrastructure/Attributes/PrimaryKeyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fabrikam.Infrastructure.Attributes
+{
+    public class PrimaryKeyAttribute : Attribute
+    {
+        public string ColumnName { get; set; }
+
+        public PrimaryKeyAttribute()
+        {
+            this.ColumnName = string.Empty;
+        }
+
+        public PrimaryKeyAttribute(string columnName)
+        {
+            this.ColumnName = columnName;
+        }
+
+        public string GetColumnName()
+        {
+            return ColumnName;
+        }
+    }
+}
