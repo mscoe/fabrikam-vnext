6b1d2eab32bd09db1fe1da09849d4632a3737adf
arunv7 <arunvkumar7@gmail.com>
2015-09-11 15:58:35 +0530

    Added angularjs with require js

---
+++ src/Fabrikam.Web.Ui/wwwroot/_references.js
@@ -0,0 +1,5 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="core/app.js" />
+/// <reference path="core/requireConfig.js" />
+/// <reference path="main.js" />
+/// <reference path="services/AjaxServices.js" />
---
+++ src/Fabrikam.Web.Ui/wwwroot/core/appConfig.js
@@ -0,0 +1,11 @@
+﻿define(['angular'], function (angular) {
+    var config= angular.module('app.Config', []);
+    var initInjector = angular.injector(['ng']);
+    var $http = initInjector.get('$http');
+    $http.get('/core/constants.json').then(
+             function (response) {
+                 config.value('config', response.data);
+             }
+         );
+    return config;
+});
