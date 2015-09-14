3c18cbe34c4d30e2a5f34d08a5b17bf1d52fac17
Karthik H Raju <karthik.h.raju@gmail.com>
2015-09-14 10:40:01 +0530

    Added some stub interfaces and classes. Repository classes have been added

---
+++ src/Fabrikam.Module1.Uc1.Command.Business/CreateAirportLocationCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Fabrikam.Command.Entities.Business.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fabrikam.Module1.Uc1.Command.Business
+{
+    public class CreateAirportLocationCommandHandler : ICommandHandler<CreateAirportLocation>
+    {
+        private IRepository<DiaryItem> _repository;
+
+        public CreateItemCommandHandler(IRepository<DiaryItem> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(CreateItemCommand command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException("command");
+            }
+            if (_repository == null)
+            {
+                throw new InvalidOperationException("Repository is not initialized.");
+            }
+            var aggregate = new DiaryItem(command.Id, command.Title, command.Description, command.From, command.To);
+            aggregate.Version = -1;
+            _repository.Save(aggregate, aggregate.Version);
+        }
+    }
+}
+}
--- src/Fabrikam.Module1.Uc1.Command.ServiceLib.Interfaces/IAirportLocationGateway.cs
+++ src/Fabrikam.Module1.Uc1.Command.ServiceLib.Interfaces/IAirportLocationGateway.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Fabrikam.Module1.Uc1.Command.Business
+namespace Fabrikam.Module1.Uc1.Command.ServiceLib.Interfaces
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Class1
+    public interface IAirportLocationGateway
     {
-        public Class1()
-        {
-        }
     }
 }
