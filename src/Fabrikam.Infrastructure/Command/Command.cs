6c8dae67289895b9320c73cab4e166b1f6e86128
Ankit <ankit.sinha@mindtree.com>
2015-09-16 22:10:37 +0530

    Added Command

---
+++ src/Fabrikam.Infrastructure/Command/Command.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fabrikam.Command.Entities.Business.Commands
+{
+    [Serializable]
+    public class Command : ICommand
+    {
+        public Guid Id { get; private set; }
+        public int Version { get; private set; }
+        public Command(Guid id, int version)
+        {
+            Id = id;
+            Version = version;
+        }
+    }
+}
