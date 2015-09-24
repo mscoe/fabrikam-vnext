c345cc2bea8aeaf3d8888cbc8384cb6df16e58db
Ankit <ankit.sinha@mindtree.com>
2015-09-24 18:31:34 +0530

    Added autofac

---
+++ src/Fabrikam.Infrastructure.Interfaces/DependencyResolver/ICommandHandlerFactory.cs
@@ -0,0 +1,13 @@
+﻿using Fabrikam.Infrastructure.Command.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fabrikam.Infrastructure.DependencyResolver.Interfaces
+{
+    public interface ICommandHandlerFactory
+    {
+        ICommandHandler<T> GetHandler<T>() where T : ICommand;
+    }
+}
