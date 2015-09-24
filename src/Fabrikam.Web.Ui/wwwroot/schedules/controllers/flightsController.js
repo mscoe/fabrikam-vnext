907b1a55d032021713e1a767e45d4321c08f8408
arunv7 <arunvkumar7@gmail.com>
2015-09-24 21:43:24 +0530

    Modified search form

--- src/Fabrikam.Web.Ui/wwwroot/schedules/controllers/flightsController.js
+++ src/Fabrikam.Web.Ui/wwwroot/schedules/controllers/flightsController.js
@@ -1,11 +1,15 @@
-﻿define(['angular', 'app.schedules', '../services/flightsService'], function () {
+﻿define(['angular', 'app.schedules', '../services/flightsService', '../services/airportsService'], function () {
     angular.module("app.schedules")
         .controller('flightsController',
-        ['$scope','flightsService','$filter',
-            function ($scope, flightsService,$filter) {
+        ['$scope', 'flightsService', 'airportsService', '$filter',
+            function ($scope, flightsService, airportsService, $filter) {
+                $scope.airports = [];
+                $scope.flights = [];
                 $scope.data = [];
                 $scope.filteredData = [];
                 $scope.searchText = '';
+                $scope.originInSearchQuery = '';
+                $scope.destinationInSearchQuery = '';
 
                 //$scope.fetchFlights();
 
@@ -13,13 +17,41 @@
                     $scope.filteredData = $filter('filter')($scope.data, $scope.searchText, undefined);
                 };
 
+                airportsService.list().then(function (data) {
+                    $scope.airports = data.List;
+                    $scope.originInSearchQuery = data.List[0].Id;
+                    $scope.destinationInSearchQuery = data.List[1].Id;
+                });
+
                 $scope.fetchFlights = function () {
                     flightsService.fetch().then(function (data) {
                         $scope.data = data.flights;
+                        $scope.flights = data.flights;
                         $scope.filteredData = $scope.data;
                     })
                 }
             }
         ]
-        );
-});
+        ).directive('footableBinding', function () {
+            return function (scope, element) {
+                var footableTable = element.parents('table');
+
+
+                if (!scope.$last) {
+                    return false;
+                }
+
+                scope.$evalAsync(function () {
+
+                    if (!footableTable.hasClass('footable-loaded')) {
+                        footableTable.footable();
+                    }
+
+                    footableTable.trigger('footable_initialized');
+                    footableTable.trigger('footable_resize');
+                    footableTable.data('footable').redraw();
+
+                });
+            };
+        });
+})
---
+++ src/Fabrikam.Web.Ui/wwwroot/schedules/services/airportsService.js
@@ -0,0 +1,20 @@
+﻿define(['angular', 'app.schedules'], function () {
+    angular.module("app.schedules").factory('airportsService', function ($q, $timeout, $http) {
+        var airportsService = {
+            list: function () {
+
+                var deferred = $q.defer();
+
+                $timeout(function () {
+                    $http.get('schedules/services/airports.json').success(function (data) {
+                        deferred.resolve(data);
+                    });
+                }, 30);
+
+                return deferred.promise;
+            }
+        }
+
+        return airportsService;
+    });
+});
