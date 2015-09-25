6e85229d943286a9201b6dc0b34f2808f9bb96b6
Ankit <ankit.sinha@mindtree.com>
2015-09-25 10:20:54 +0530

    Added autofac container

---
+++ src/Fabrikam.Infrastructure.DependencyResolver/AutofacContainer.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Fabrikam.Infrastructure.DependencyResolver.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fabrikam.Infrastructure.Command.Interface;
+
+namespace Fabrikam.Infrastructure.DependencyResolver.Autofac
+{
+    public class AutofacCommandHandlerFactory: ICommandHandlerFactory
+    {
+        private ILifetimeScope _container;
+        public AutofacCommandHandlerFactory(ILifetimeScope container)
+        {
+            if (container == null)
+                throw new ArgumentNullException("Autofac Container not present.");
+            _container = container;
+        }
+
+        public ICommandHandler<T> GetHandler<T>() where T : ICommand
+        {
+            return _container.Resolve<ICommandHandler<T>>();
+        }
+    }
+}
