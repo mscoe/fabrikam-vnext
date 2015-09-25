67e5816c7ae024c95c8d8bdb37c5d857b9cccb32
Ankit <ankit.sinha@mindtree.com>
2015-09-25 12:26:30 +0530

    tried to add autofac

--- src/Fabrikam.Command.Entities.Business/Commands/CreateAirportLocationCommand.cs
+++ src/Fabrikam.Command.Entities.Business/Commands/CreateAirportLocationCommand.cs
@@ -4,7 +4,7 @@ namespace Fabrikam.Command.Entities.Business.Commands
 {
     // This project can output the Class library as a NuGet Package.
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class CreateAirportLocationCommand : Command
+    public class CreateAirportLocationCommand : Fabrikam.Infrastructure.Command.Command
     {
 
         public int AirportLocationId { get; set; }
@@ -13,7 +13,7 @@ namespace Fabrikam.Command.Entities.Business.Commands
 
         public string AirportCode { get; set; }
 
-        public CreateAirportLocationCommand(Guid aggregateId, int airportLocationId, string airportName, string airportCode, int version) : base(aggregateId, version)
+        public CreateAirportLocationCommand(string aggregateId, int airportLocationId, string airportName, string airportCode, int version) : base(aggregateId, version)
         {
             AirportLocationId = airportLocationId;
             AirportName = airportName;
---
+++ src/Fabrikam.Module1.Uc1.Command.WebApi/DependencyResolver/AutofacResolver.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Framework.Runtime;
+
+namespace Fabrikam.Module1.Uc1.Command.WebApi.DependencyResolver
+{
+    public class AutofacResolver : IAutofacResolver
+    {
+        private readonly ContainerBuilder builder;
+
+        public AutofacResolver()
+        {
+            builder = new ContainerBuilder();
+        }
+
+        public void RegisterLibraryModules(ILibraryManager libraryManager)
+        {
+            IEnumerable<ILibraryInformation> libraryInformations = libraryManager.GetLibraries();
+
+            IEnumerable<AssemblyName> assemblyNames = libraryInformations.SelectMany(e => e.LoadableAssemblies).Distinct();
+            // assemblyNames = assemblyNames.Where(e => e.Name.StartsWith("")); // Filter Assemblies
+
+            List<Assembly> assemblies = new List<Assembly>();
+
+            foreach (AssemblyName assemblyName in assemblyNames)
+            {
+                Assembly assembly = Assembly.Load(assemblyName);
+                assemblies.Add(assembly);
+            }
+
+            this.builder.RegisterAssemblyModules(assemblies.ToArray());
+        }
+
+        public void Populate(IServiceCollection services)
+        {
+            builder.Populate(services);
+        }
+
+        public IServiceProvider GetServiceProvider()
+        {
+            IContainer container = this.builder.Build();
+            return container.Resolve<IServiceProvider>();
+        }
+    }
+}
