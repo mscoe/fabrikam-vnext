3bf8fcc311dfe266d0eed0629e19aa7e9ac6184d
surbhigarg1992 <M1023900@mindtree.com>
2015-10-15 16:34:07 +0530

    Added spec files and service file for testing

---
+++ src/Fabrikam.Web.Ui/wwwroot/schedules/services/flightscheduleService.js
@@ -0,0 +1,23 @@
+﻿define(['angular', 'app.schedules'], function () {
+    angular.module("app.schedules").factory('flightscheduleService', function ($q, $timeout, httpgetService) {
+        var flightscheduleService = {
+            fetch: function () {
+
+                var deferred = $q.defer();
+
+                //$timeout(function () {
+                    var data = httpgetService.get('schedules/services/flightschedule.json', function success(data) {
+                        deferred.resolve(data);
+                    //}, function error(data) {
+                    //    deferred.reject('error');
+                    });
+                //}, 30);
+
+                return deferred.promise;
+            }
+        }
+
+        return flightscheduleService;
+
+    });
+});
---
+++ src/Fabrikam.Web.Ui/wwwroot/schedules_test/services_Specs/httpgetServicespec.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+define(['angular', 'app.schedules','app.schedules.mock', 'angularMocks', 'schedules/services/httpgetService'], function (angular) {
+    describe('httpgetService tests', function () {
+
+        beforeEach(module('app.schedules'));
+
+        var httpgetMockservice;
+
+        beforeEach(module('app.schedules.mock'));
+
+        beforeEach(inject(function (httpgetServiceMock) {
+            httpgetMockservice = httpgetServiceMock;
+        }));
+
+
+        describe('get function', function () {
+
+            it('get result from server', function () {
+                var data1;
+
+                httpgetMockservice.get('schedules/services/flights.json', function success(data) {
+
+                    //console.log(data.flights[0]);
+                    data1 = data;
+                    return data;
+                });
+                //console.log(data1.flights[0]);
+                expect(data1).not.toBe(null);
+            });
+        });
+    });
+});
